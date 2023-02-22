fe7dde11509057b4279300823e826c5e3cbbc7b2
Konstantin Markov <maga.ot.oz@protonmail.com>
2023-02-22 11:50:47 +0200

    Remove planning

--- client/index.js
+++ client/index.js
@@ -15,10 +15,6 @@ setTimeout(() => {
                 id: 'datetimeFieldExtension',
                 load: () => import('superdesk-core/scripts/extensions/datetimeField'),
             },
-            {
-                id: 'planning-extension',
-                load: () => import('superdesk-planning/client/planning-extension'),
-            },
         ],
         {},
     );
